986afeddd4c6b93acd642960a59811d0d5e0e227
shitalfating <shitalfating84@gmail.com>
2023-01-12 15:20:51 +0530

    Welcome to Logical Program

---
+++ src/com/bridgelabz/LogicalProgram/PrimeNumber.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.LogicalProgram;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = scanner.nextInt();
+        int x=4;
+        for(int i=2;i<x;i++){
+            System.out.println("The given num is prime");
+        }
+    }
+    }
+}
